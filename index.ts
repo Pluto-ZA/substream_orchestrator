f708b9da74f00bd67b0782b243406d7493dc4a6c
ColourfulMelon <jordynnitch@gmail.com>
2026-01-12 10:25:21 +0200

    reset start block

--- index.ts
+++ index.ts
@@ -217,7 +217,7 @@ async function startSubstream() {
             substreamPackage,
             outputModule: OUTPUT_MODULE,
             productionMode: true,
-            startBlockNum: 390251284,//376967294,
+            startBlockNum: 376967294,
             startCursor: startCursor
         });
         
