88d94bfcdfadb05148df07c6cbe90802d8cbb78e
ColourfulMelon <jordynnitch@gmail.com>
2026-02-26 00:35:35 +0200

    add build command

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "wallet-balances",
-        script: "./index.js",
+        script: "./build/index.js",
         env: {
             NODE_ENV: "production",
         }
