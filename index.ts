1253a7b2392de0df12cb2afd956693574fee60ef
ColourfulMelon <jordynnitch@gmail.com>
2026-04-03 12:43:44 +0200

    update database name to 'sink'

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const clickhouse = createClient({
     url: process.env.CLICKHOUSE_URL!,
     username: 'default',
     password: process.env.CLICKHOUSE_PASSWORD!,
-    database: 'solana',
+    database: 'sink',
     
     request_timeout: 60_000,
     // 3. Disable keep_alive to prevent reusing dead sockets
